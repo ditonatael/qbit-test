ece61fd8c0afc18d21092c02fa2e564471cd9239
mac <mac@macs-MacBook-Pro.local>
2025-04-23 18:12:59 +0700

    feat: Header navigation

--- case03/src/components/Header/Sheet.tsx
+++ case03/src/components/Header/Sheet.tsx
@@ -4,18 +4,27 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "~/components/ui/sheet";
+import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function MobileSheet() {
   return (
     <Sheet>
-      <SheetTrigger>Open</SheetTrigger>
+      <SheetTrigger>
+        <GiHamburgerMenu />
+      </SheetTrigger>
       <SheetContent>
-        {" "}
         <SheetTitle></SheetTitle>
-        <div className="flex flex-col mt-7 gap-4">
-          <span className="h-10 border-b pl-1.5 font-light">Home</span>
-          <span className="h-10 border-b pl-1.5 font-light">Products</span>
-          <span className="h-10 border-b pl-1.5 font-light">About</span>
+        <div className="flex flex-col mt-7 gap-4 ">
+          <Link href={"/"} className="w-full">
+            <div className="h-12 pl-4 border-b w-full">Home</div>
+          </Link>
+          <Link href={"/products"} className="w-full">
+            <div className="h-12 pl-4 border-b w-full">Products</div>
+          </Link>
+          <Link href={"/about"} className="w-full">
+            <div className="h-12 pl-4 border-b w-full">About</div>
+          </Link>
         </div>
       </SheetContent>
     </Sheet>
--- case03/src/components/Header/index.tsx
+++ case03/src/components/Header/index.tsx
@@ -1,17 +1,26 @@
 import { PiCoffeeFill } from "react-icons/pi";
 import MobileSheet from "./Sheet";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="h-14 border-b border-zinc-200">
       <div className="h-full container mx-auto flex items-center justify-between px-4">
-        <h1 className="font-bold text-xl flex items-center gap-0.5">
-          <PiCoffeeFill /> KopiKampung
-        </h1>
+        <Link href={"/"}>
+          <h1 className="font-bold text-xl flex items-center gap-0.5">
+            <PiCoffeeFill /> KopiKampung
+          </h1>
+        </Link>
         <div className="hidden md:flex gap-4 text-zinc-700">
-          <span>Home</span>
-          <span>Products</span>
-          <span>About</span>
+          <Link href={"/"}>
+            <span>Home</span>
+          </Link>
+          <Link href={"/products"}>
+            <span>Products</span>
+          </Link>
+          <Link href={"/about"}>
+            <span>About</span>
+          </Link>
         </div>
         <div className="block md:hidden">
           <MobileSheet />
