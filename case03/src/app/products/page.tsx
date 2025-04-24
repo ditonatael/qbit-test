914f63dfa07339fcdabf900bdbf1b46484478af4
mac <mac@macs-MacBook-Pro.local>
2025-04-24 09:06:22 +0700

    fix: products page description

--- case03/src/app/products/page.tsx
+++ case03/src/app/products/page.tsx
@@ -6,8 +6,8 @@ export default function Products() {
       <div className="py-20 text-center">
         <h2 className="text-5xl font-semibold mb-4">Our Coffee Products</h2>
         <span className="text-sm font-semibold text-zinc-500">
-          Discover our carefully curated selection of premium coffee beans,
-          sourced from the <br /> finest regions around Indonesia.
+          A journey of taste begins here — premium beans <br /> from the heart
+          of Indonesia’s rich coffee heritage.
         </span>
       </div>
       <ProductList />
